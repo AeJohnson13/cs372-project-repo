9d1b9a817594cce6151b14539e4a028c3391a7d3
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-04-18 11:23:06 -0800

    Tried to throw together an admin page

--- JavaScript/adminPageScripts.js
+++ JavaScript/adminPageScripts.js
@@ -2,6 +2,55 @@
 // Alex Johnson, Ryland Sacker, Enica King
 // Scripts for adminPage.html
 
+async function addButton()
+{
+  try {
+        const response = await fetch("/getUserList");
+        const users = await response.json();
+    
+        const usersList = document.getElementById("usersList");
+        usersList.innerHTML = ""; // Clear any existing items
+    
+        users.forEach(user => {
+          const div = document.createElement("div");
+          div.className = "parent-flex";
+          const header = document.createElement("h2");
+          header.innerText = user.username;
+          div.appendChild(header);
+
+          const viewerButton = document.createElement("button");
+          viewerButton.innerHTML = "viewer";
+          if(user.roles.viewer == 0){
+            viewerButton.className ="offButton";
+          }
+          const adminButton = document.createElement("button");
+          adminButton.innerHTML = "administrator";
+          if(user.roles.admin == 0){
+            adminButton.className ="offButton";
+          }
+          const markManButton = document.createElement("button");
+          markManButton.innerHTML = "marketing manager";
+          if(user.roles.markman == 0){
+            markManButton.className ="offButton";
+          }
+          const contManButton = document.createElement("button");
+          contManButton.innerHTML = "content manager";
+          if(user.roles.contman == 0){
+            contManButton.className = "offButton";
+          }
+
+          div.appendChild(viewerButton);
+          div.appendChild(adminButton);
+          div.appendChild(markManButton);
+          div.appendChild(contManButton);
+          
+          usersList.appendChild(div);
+        });
+  
+      } catch (err) {
+        console.error("Error loading user list:", err);
+      }
+  }
 
 
 async function displayData() {
@@ -17,7 +66,7 @@ async function displayData() {
     });
 }
 
-export async function showAdminTools() {
+async function showAdminTools() {
     try {
       const response = await fetch("/getUserList");
       const users = await response.json();
@@ -50,3 +99,7 @@ return Object.entries(roles)
     .map(([role]) => role)
     .join(', ') || 'None';
 }
+
+document.addEventListener('DOMContentLoaded', async () => {
+  addButton();
+});
